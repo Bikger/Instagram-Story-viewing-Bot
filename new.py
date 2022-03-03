f7254ed7cf6238ea6c3ccee86bf4e2117b6badf7
mesickerim <95288167+Bikger@users.noreply.github.com>
2022-03-03 18:58:39 +0100

    Code refactoring, initialised as a class

--- new.py
+++ new.py
@@ -1,59 +1,100 @@
+import ssl
+import urllib
+from datetime import datetime
+import nasapy
+import os
+from json import dump, load
 from selenium import webdriver
 from time import sleep
-import time
+from os.path import exists
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-def view_stories(username, password):
-    driver = webdriver.Firefox()
-    driver.implicitly_wait(5)
-    driver.get("https://instagram.com/")
+class InstaBot:
+    '''Set driverpath to geckodriver file path to be able to use Firefox'''
+    driverpath = None
 
-    # login_link = browser.find_element_by_xpath("//a[text()='Log in']")
-    # login_link.click()
+    def __init__(self):
+        self.driver = webdriver.Firefox()
 
-    sleep(2)
-
-    username_input = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
-    password_input = driver.find_element(By.CSS_SELECTOR,"input[name='password']")
-
-    username_input.send_keys(username)
-    password_input.send_keys(password)
-
-    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-    login_button.click()
-
-    sleep(4)
-    print("Report: ")
-    try:
-        not_now_button = driver.find_element(By.XPATH, "//button[text()='Ne sada']")
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(not_now_button)).click()
-    except:
-        print("'not now' button not found.")
-
-    sleep(2)
-
-    try:
-        notifications_button = driver.find_element(By.XPATH,'//button[text()="Ne sada"]')
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(notifications_button)).click()
-    except:
-        print("Didn't find notifications button")
-
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@aria-label, 'Priča')]"))).click()
-
-    try:
-        while True:
+    def login(self, username, password):
+        self.driver.get("https://instagram.com/")
+        self.driver.implicitly_wait(7)
+        if not exists("./Cookies/ig.json"):
             try:
-                NextStory = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'FhutL')]")))
-                NextStory.click()
+                os.makedirs('Cookies')
             except:
-                break
-    except:
-        print("Did not click on next")
+                print("File 'Cookies' exists. ")
+            try:
+                username_input = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
+                password_input = self.driver.find_element(By.CSS_SELECTOR, "input[name='password']")
+                username_input.send_keys(username)
+                password_input.send_keys(password)
+                cookie = self.driver.get_cookies()
 
-    unixTimeStamp = time.time()
-    driver.quit()
-    print(unixTimeStamp, "\nNo issues encountered.")
-    
-view_stories("username", "password")
+                login_button = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+                login_button.click()
+                not_now_button = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Ne sada']")))
+                not_now_button.click()
+                notifications_button = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Ne sada"]')))
+                notifications_button.click()
+
+                with open("Cookies/ig.json", "w") as file:
+                    dump(cookie, file)
+                print("Cookies saved!")
+
+                self.driver.refresh()
+
+            except:
+                raise KeyError("Error! Login failed.")
+        else:
+            '''
+            '''
+            with open("./Cookies/ig.json", "r") as _file:
+                for i in load(_file):
+                    self.driver.add_cookie(i)
+
+                sleep(1)
+                self.driver.refresh()
+
+                try:
+                    username_input = WebDriverWait(self.driver, 20).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
+                    password_input = self.driver.find_element(By.CSS_SELECTOR, "input[name='password']")
+                    username_input.send_keys(username)
+                    password_input.send_keys(password)
+                    cookie = self.driver.get_cookies()
+
+                    login_button = WebDriverWait(self.driver, 20).until(
+                        EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+                    login_button.click()
+                    not_now_button = WebDriverWait(self.driver, 20).until(
+                        EC.element_to_be_clickable((By.XPATH, "//button[text()='Ne sada']")))
+                    not_now_button.click()
+                    notifications_button = WebDriverWait(self.driver, 20).until(
+                        EC.element_to_be_clickable((By.XPATH, '//button[text()="Ne sada"]')))
+                    notifications_button.click()
+                except:
+                    raise Exception("Login failed! ")
+
+                print("Login successful")
+
+
+    def cycle_stories(self):
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(@aria-label, 'Priča')]"))).click()
+        try:
+            while True:
+                try:
+                    NextStory = WebDriverWait(self.driver, 2).until(
+                        EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'FhutL')]")))
+                    NextStory.click()
+                except:
+                    break
+        except:
+            print("Did not click on next")
+
+instance = InstaBot()
+instance.login(username, password)
+instance.cycle_stories()
